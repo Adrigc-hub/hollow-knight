6b48420d22d20b26d6a2eac1d2847f07cdc77365
Adrigc-hub <adrigc1214@gmail.com>
2026-06-25 02:27:41 -0600

    Fix: Update game.gd to Godot 4 FileAccess API

--- src/game.gd
+++ src/game.gd
@@ -1,49 +1,56 @@
 # Global singleton (Autoload) - store abilities, checkpoint & simple save
+# Godot 4 version
 extends Node
 
 var abilities := {}
-var save_path := "user://save.dat"
-var checkpoint_position := null
+var save_path := "user://hollow_knight_save.dat"
+var checkpoint_position := Vector2.ZERO
 
 func _ready():
-    load_game()
+	load_game()
 
 func give_ability(key: String):
-    abilities[key] = true
-    save_game()
-    # notify scenes
-    get_tree().call_group("gates", "update_state")
+	abilities[key] = true
+	save_game()
+	# notify scenes
+	get_tree().call_group("gates", "update_state")
 
 func has_ability(key: String) -> bool:
-    return abilities.get(key, false)
+	return abilities.get(key, false)
 
 func set_checkpoint_position(pos: Vector2):
-    checkpoint_position = pos
-    save_game()
+	checkpoint_position = pos
+	save_game()
 
 func get_checkpoint_position() -> Vector2:
-    if checkpoint_position:
-        return checkpoint_position
-    return Vector2.ZERO
+	if checkpoint_position:
+		return checkpoint_position
+	return Vector2.ZERO
 
 func save_game():
-    var file := File.new()
-    file.open(save_path, File.WRITE)
-    var data := {
-        "abilities": abilities,
-        "checkpoint": checkpoint_position
-    }
-    file.store_var(data)
-    file.close()
+	var data := {
+		"abilities": abilities,
+		"checkpoint": checkpoint_position
+	}
+	var json_string = JSON.stringify(data)
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	if file:
+		file.store_string(json_string)
 
 func load_game():
-    var file := File.new()
-    if file.file_exists(save_path):
-        file.open(save_path, File.READ)
-        var data = file.get_var()
-        file.close()
-        abilities = data.get("abilities", {})
-        checkpoint_position = data.get("checkpoint", null)
-    else:
-        abilities = {}
-        checkpoint_position = null
+	if ResourceLoader.exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		if file:
+			var json_string = file.get_as_text()
+			var json = JSON.new()
+			json.parse(json_string)
+			var data = json.get_data()
+			if data:
+				abilities = data.get("abilities", {})
+				checkpoint_position = data.get("checkpoint", Vector2.ZERO)
+			else:
+				abilities = {}
+				checkpoint_position = Vector2.ZERO
+	else:
+		abilities = {}
+		checkpoint_position = Vector2.ZERO
